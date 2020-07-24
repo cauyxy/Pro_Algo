302de359cf083f77a3b33f6bfcb302c011cf4997
ZhY <54785955+zy-zzf2000@users.noreply.github.com>
2020-07-25 00:07:43 +0800

    新增一个参数用于传递比较函数

--- Sort/MergeSort.py
+++ Sort/MergeSort.py
@@ -1,11 +1,13 @@
+from models import *
+from cmps import *
 ###归并排序
 
-def merge(arr, l, r, m):    #合并函数
+def merge(arr, l, r, m,cmp):    #合并函数
     n1 = m - l + 1
     n2 = r - m
 
-    L = [0 for _ in range(0, n1)]     
-    R = [0 for _ in range(0, n2)]     
+    L = [0 for _ in range(0, n1)]
+    R = [0 for _ in range(0, n2)]
 
     for i in range(0, n1):     #L存储待排序数组的左半部分
         L[i] = arr[l + i]
@@ -18,7 +20,7 @@ def merge(arr, l, r, m):    #合并函数
     k = l
 
     while i < n1 and j < n2:    #将L和R中按从小到大顺序移到数组中
-        if L[i] < R[j]:
+        if cmp(L[i],R[j]):
             arr[k] = L[i]
             i += 1
             k += 1
@@ -38,9 +40,11 @@ def merge(arr, l, r, m):    #合并函数
         j += 1
 
 
-def MergeSort(arr, l, r):      #递归进行归并排序
+def MergeSort(arr, l, r,cmp):      #递归进行归并排序
     if l < r:
         m = int((l + r - 1) / 2)
-        MergeSort(arr, l, m)
-        MergeSort(arr, m + 1, r)
-        merge(arr, l, r, m)
+        MergeSort(arr, l, m,cmp)
+        MergeSort(arr, m + 1, r,cmp)
+        merge(arr, l, r, m,cmp)
+    return arr
+
