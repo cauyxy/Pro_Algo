05668cd67137779b24df108f9e0d2524ae4f3939
ZhY <54785955+zy-zzf2000@users.noreply.github.com>
2020-07-24 21:05:37 +0800

    Update QuickSort.py

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -2,14 +2,14 @@
 
 def QuickSort(arr):
     length=len(arr)
-    if length < 2:  # 为空或只有一个节点的数组为有序的
+    if length < 2:  # 为空或只有一个节点的数组为有序的数组
         return arr
-    pivot = arr[0]
-    less = []
-    greater = []
-    for i in range(1,length):
+    pivot = arr[0]  #每次均选取数组第一个作为基准元素
+    less = []      
+    greater = []   
+    for i in range(1,length):     #分别将比基准元素小的和大的元素存储到less和greater中
         if arr[i]<=pivot:
             less.append(arr[i])
         else:
             greater.append(arr[i])
-    return QuickSort(less)+[pivot]+QuickSort(greater)
+    return QuickSort(less)+[pivot]+QuickSort(greater)    #递归进行排序
