03666e6ee4c76e58d9dc9f692bb78a2346479484
ZhY <54785955+zy-zzf2000@users.noreply.github.com>
2020-07-25 00:08:24 +0800

    新增一个参数用于传递比较函数

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -1,15 +1,20 @@
 ###快排
+from models import *
+from cmps import *
 
-def QuickSort(arr):
+
+def QuickSort(arr,cmp):
     length=len(arr)
     if length < 2:  # 为空或只有一个节点的数组为有序的数组
         return arr
     pivot = arr[0]  #每次均选取数组第一个作为基准元素
-    less = []      
-    greater = []   
+    less = []
+    greater = []
     for i in range(1,length):     #分别将比基准元素小的和大的元素存储到less和greater中
-        if arr[i]<=pivot:
+        if cmp(arr[i],pivot):
             less.append(arr[i])
         else:
             greater.append(arr[i])
-    return QuickSort(less)+[pivot]+QuickSort(greater)    #递归进行排序
+    return QuickSort(less,cmp)+[pivot]+QuickSort(greater,cmp)    #递归进行排序
+
+
