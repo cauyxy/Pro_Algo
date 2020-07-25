186a5ed02c64254e2bcc54ebb5af85ca8abc0dbc
GGG-c <57121573+GGG-c@users.noreply.github.com>
2020-07-25 21:44:57 +0800

    DFS遍历--1

--- Search/FindPre.py
+++ Search/FindPre.py
@@ -4,7 +4,7 @@ def DFS(node,PreNodeList):
     for Pre_node in node.pre_list:
         if(Pre_node not in PreNodeList):
             PreNodeList.append(Pre_node)
-            FindPre(Pre_node,PreNodeList)
+            DFS(Pre_node,PreNodeList)
 def FindPre(node):
     PreNodeList = []
     DFS(node,PreNodeList)
