ef141b2bcad1572dd0f5ecd850269dcc706849b3
GGG-c <57121573+GGG-c@users.noreply.github.com>
2020-07-25 21:37:38 +0800

    DFS遍历--1

--- Search/FindPre.py
+++ Search/FindPre.py
@@ -1,12 +1,13 @@
 from models import load2prog
 nodes=load2prog("../data.dat")
-PreNodeList=[]
-def DFS(node):
+def DFS(node,PreNodeList):
     for Pre_node in node.pre_list:
         if(Pre_node not in PreNodeList):
             PreNodeList.append(Pre_node)
-            FindPre(Pre_node)
+            FindPre(Pre_node,PreNodeList)
 def FindPre(node):
-    DFS(node)
+    PreNodeList = []
+    DFS(node,PreNodeList)
     return PreNodeList
+
 
