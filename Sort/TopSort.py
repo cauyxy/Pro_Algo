0ad7107509d20d8659997a7dab4accb84a568905
ZhY <2018304010314@cau.edu.cn>
2020-07-26 19:14:06 +0800

    修改函数传递参数，方便封装时调用

--- Sort/TopSort.py
+++ Sort/TopSort.py
@@ -1,14 +1,13 @@
 from models import *
 from Search.FindPre import FindPre
 ####拓扑排序
-
 import models
 import heapq #用到了python中的堆
+heap = []  # 用于存储节点的优先队列
 
-heap = []   #用于存储节点的优先队列
-nums = []     #用于存储各个节点的入度
 
-def TopSort(n,nodes):
+
+def TopSort(n,nodes,nums):
     tol_time=0
     ans = []
     for node in nodes:
@@ -27,24 +26,9 @@ def TopSort(n,nodes):
 
 
 
-nodes=load2prog("../data.dat")
-
-
-pre_list=FindPre(nodes[-3])
-for nodes in pre_list:
-    print(nodes.name)
-
-
-length=len(pre_list)
-nums = [float('inf') for _ in range(20)]
-
-for node in pre_list:
-    nums[node.id-1]=len(node.pre_list)
 
 
 
-time,ans=TopSort(length,pre_list)
-print(time,ans)
 
 
 
