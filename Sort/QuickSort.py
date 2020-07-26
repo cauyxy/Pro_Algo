e0e71da7bd329ed1b62eddd3f606ea665bddb265
ZhY <953725892@qq.com>
2020-07-26 19:13:23 +0800

    修改函数传递的参数，方便封装时调用

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -1,9 +1,9 @@
-###快排
-from models import *
+###快排nlog，数据量较大时，速度快于归并排序，同样需要额外的空间开销
+import models
 from cmps import *
+from Shortest_path.Dijskra import Dijsktra
 
-
-def QuickSort(arr,cmp):
+def QuickSort(arr,cmp,dis=[]):
     length=len(arr)
     if length < 2:  # 为空或只有一个节点的数组为有序的数组
         return arr
@@ -11,10 +11,10 @@ def QuickSort(arr,cmp):
     less = []
     greater = []
     for i in range(1,length):     #分别将比基准元素小的和大的元素存储到less和greater中
-        if cmp(arr[i],pivot):
+        if cmp(arr[i],pivot,dis):
             less.append(arr[i])
         else:
             greater.append(arr[i])
-    return QuickSort(less,cmp)+[pivot]+QuickSort(greater,cmp)    #递归进行排序
+    return QuickSort(less,cmp,dis)+[pivot]+QuickSort(greater,cmp,dis)    #递归进行排序
 
 
