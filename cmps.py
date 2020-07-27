6482be555e6c1633695d5286c28317e55929a44d
ZhY <2018304010314@cau.edu.cn>
2020-07-27 12:06:15 +0800

    新增cmp3，用于比较相似度大小

--- cmps.py
+++ cmps.py
@@ -1,7 +1,24 @@
 from models import *
 
-def cmp1(node1,node2):
-    return node1.level <= node2.level
 
-def cmp2(node1,node2):
-    return node1.time <= node2.time
+def cmp1(node1,node2,dis=[]):
+    if node1.level!=node2.level:
+        return node1.level<node2.level
+    else:
+        return node1.id<node2.id
+
+def cmp2(node1,node2,dis=[]):
+    if node1.time != node2.time:
+        return node1.time < node2.time
+    else:
+        return node1.id < node2.id
+
+def cmp3(node1,node2,dis=[]):     #dis为用SFPA算法或dijskra算法计算出的到各个节点的最短距离
+    if dis[node1.id-1]!=dis[node2.id-1]:
+        return dis[node1.id-1]<=dis[node2.id-1]
+    elif node1.level!=node2.level:
+        return node1.level<=node2.level
+    elif node1.time!=node2.time:
+        return node1.time<=node2.time
+    else:
+        return node1.id<=node2.id
