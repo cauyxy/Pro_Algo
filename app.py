dcd27dd250bd4b9a28fbcde3b9c6368ab9311f63
Seongjang <cauyxy@163.com>
2020-07-27 11:38:45 +0800

    完成Flask的初步搭建

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import *
 
 app = Flask(__name__)
+
+Nodes = load2prog("data.dat")
+
+
+def get_Node(id):
+    for node in Nodes:
+        if id == node.id:
+            return node
 
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", nodes_part1=Nodes[:10], nodes_part2=Nodes[10:])
+
+
+@app.route('/details', methods=["get", "post"])
+def details():
+    node_id = request.values.get("id")
+    node = get_Node(eval(node_id))
+
+    # TODO: 完成sim_node的计算
+    sim_nodes = 0
+
+    return render_template("single.html", node=node)
+
 
 if __name__ == '__main__':
-    app.run(port=8887,host="0.0.0.0")
+    app.run(port=8887)
