79025cd762e93e433bc8c6788c3fd9be36ae429f
GGG-c <57121573+GGG-c@users.noreply.github.com>
2020-07-27 14:53:59 +0800

    综合搜索算法 输入为字符串 返回node list

--- Search/CutWord.py
+++ Search/CutWord.py
@@ -6,8 +6,7 @@ def CutWord(str):
     str_cut=jieba.cut(str)
     str_cut={}.fromkeys(str_cut).keys()
     for word in str_cut:
-        if(len(word)>=2):
-            res_pinyin.append(''.join(lazy_pinyin(word)))
-            res_word.append(word)
+        res_pinyin.append(''.join(lazy_pinyin(word)))
+        res_word.append(word)
 
     return res_word,res_pinyin
--- Search/Levenshtein.py
+++ Search/Levenshtein.py
@@ -1,6 +1,5 @@
 #coding:utf-8
-from Search.CutWord import CutWord
-from pypinyin import lazy_pinyin
+
 '''
 Levenshtein算法
 动态规划实现
@@ -24,15 +23,4 @@ def Levenshtein(input, target):
                 dp[i][j] = dp[i - 1][j - 1]
             else:
                 dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
-    return dp[-1][-1]
-
-if __name__=="__main__":
-    input= "数据结构"
-    target="数据结构是一门很厉害的课程"
-    if(len(input)>1):
-        input_pinyin=''.join(lazy_pinyin(input))
-        target_word,target_pinyin=CutWord(target)
-        for target_word_pinyin in target_pinyin:
-            print(input_pinyin)
-            print(target_word_pinyin)
-            print(Levenshtein(input_pinyin, target_word_pinyin))
+    return dp[-1][-1]+2
