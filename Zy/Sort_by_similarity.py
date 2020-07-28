46da0ab56b6c90c12f89a008cba243895957bf7c
ZhY <2018304010314@cau.edu.cn>
2020-07-29 07:12:18 +0800

    读取文件路径设置

--- Zy/Sort_by_similarity.py
+++ Zy/Sort_by_similarity.py
@@ -5,7 +5,7 @@ import models
 #根据到所需知识点的相似度对知识点进行排序
 
 def Sort_by_similarity(node):
-    nodes = models.load2prog('../data.dat')
+    nodes = models.load2prog('data.dat')
     edgs = models.To_Graph()
     dis = Dijsktra(edgs, node.id-1)
     sorted_nodes = QuickSort(nodes, cmp3, dis=dis)
