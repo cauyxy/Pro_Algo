8d2fbfb8c397b745e08ceb8924b73809679cd682
ZhY <2018304010314@cau.edu.cn>
2020-07-29 17:25:45 +0800

    修改路径

--- Zy/Sort_by_time.py
+++ Zy/Sort_by_time.py
@@ -3,11 +3,9 @@ from cmps import cmp2
 import models
 #根据知识点的课时进行排序
 
-def Sort_by_level():
-    nodes=models.load2prog('../data.dat')
+def Sort_by_time():
+    nodes=models.load2prog('data.dat')
     sorted_nodes=QuickSort(nodes,cmp2)
     return sorted_nodes
 
-aft_list=Sort_by_level()
-for nodes in aft_list:
-    print(nodes.name)
+
